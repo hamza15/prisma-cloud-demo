747d67455a53c6c57e3980b8e73eccad29c482ae
sardarnaqi <sardar.naqi@sleepcountry.ca>
2023-11-12 18:54:04 -0500

    compute resources

---
+++ compute/main.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename      = "lambda.zip"
+  function_name = "checkov_lambda"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  memory_size   = 2048 # Note this
+  
+  runtime = "nodejs14.x"
+}

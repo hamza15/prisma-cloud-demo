40b08d9d5e45b494a3bba072be5b6794e3748370
sardarnaqi <sardar.naqi@sleepcountry.ca>
2023-11-12 12:10:47 -0500

    pipeline and tf structure

--- networking/main.tf
+++ networking/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "prisma_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true # Internal domain name
   enable_dns_hostnames = true # Internal host name
-  default_security_group_id = aws_default_security_group.vpc_sg.id
+  # default_security_group_id = aws_default_security_group.vpc_sg.id
 
   depends_on = [ aws_default_security_group.vpc_sg ]
 }

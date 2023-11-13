1aef449478fa11b55d02dc9a1be102998788e21c
sardarnaqi <sardar.naqi@sleepcountry.ca>
2023-11-13 10:55:19 -0500

    changes to memory of lambda

--- compute/main.tf
+++ compute/main.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "test_lambda" {
   function_name = "checkov_lambda"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
-  memory_size   = 2048 # Note this
+  memory_size   = 3072 # 3072
   
   runtime = "nodejs14.x"
 }

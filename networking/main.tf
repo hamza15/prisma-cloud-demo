21528cc1e8235236f6c3875fd6126d9138a731a2
sardarnaqi <sardar.naqi@sleepcountry.ca>
2023-11-12 23:22:15 -0500

    tf changes

--- networking/main.tf
+++ networking/main.tf
@@ -11,20 +11,20 @@ resource "aws_default_security_group" "vpc_sg" {
 
   vpc_id = aws_vpc.prisma_vpc.id
 
-  ingress {
-    protocol  = "-1"
-    self      = true
-    from_port = 0
-    to_port   = 0
-  }
+  # ingress {
+  #   protocol  = "-1"
+  #   self      = true
+  #   from_port = 0
+  #   to_port   = 0
+  # }
   
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  tags = {
-    Name = "default rules"
-  }
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = "-1"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # tags = {
+  #   Name = "default rules"
+  # }
 }
